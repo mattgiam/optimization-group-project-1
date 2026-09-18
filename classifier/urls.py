7b7b465a82f6c288163395b9ff771c45ff34afe7
Aryan Sanan <aryansanan@gmail.com>
2026-09-18 09:34:23 -0700

    Rework templates and CSS to match Saad's myapp contract (about/upload/results/login), drop superseded classifier/mnist_site build

--- classifier/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path('article/', views.article, name='article'),
-    path('classify/', views.classify, name='classify'),
-    path('classify/reset/', views.reset_classifier, name='classify_reset'),
-]
